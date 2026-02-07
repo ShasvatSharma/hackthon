90a134dcf5fa7ce0a8cddc38448e132593294101
ShasvatSharma <sharmashasvat69@gmail.com>
2026-02-07 08:41:07 +0530

    Add files via upload

---
+++ citypulse-web/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CityPulseWeb from './CityPulseWeb'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <CityPulseWeb />
+    </React.StrictMode>,
+)
