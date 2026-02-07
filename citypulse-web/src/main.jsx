08688c38daffeae9be52866ef19b1ae227c9cb9e
ShasvatSharma <sharmashasvat69@gmail.com>
2026-02-07 13:37:22 +0530

    Delete citypulse-web directory

--- citypulse-web/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CityPulseWeb from './CityPulseWeb'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <CityPulseWeb />
-    </React.StrictMode>,
-)
